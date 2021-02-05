ae4bd4bbca21d18708726d67ce5c7c494545f9ba
sangjuntechs <juntech0421@github.com>
2021-02-05 16:11:58 +0900

    페이지네이션 슬라이드 기능

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,75 +1,113 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
+import "../css/Pagination.css";
 
 const PageUl = styled.ul`
-  float:left;
+  float: left;
   list-style: none;
-  text-align:center;
-  border-radius:10px;
-  color:white;
-  padding:1px;
-  border:3px solid #ff8080;
-  background-color: rgba(0,0,0,0.5);
-  width:55%;
-  font-weight:600;
+  text-align: center;
+  border-radius: 10px;
+  color: white;
+  padding: 1px;
+  border: 3px solid #ff8080;
+  background-color: rgba(0, 0, 0, 0.5);
+  width: 55%;
+  font-weight: 600;
 `;
 
 const PageLi = styled.li`
-  display:inline-block;
-  font-size:17px;
-  font-weight:600;
-  padding:5px;
-  border-radius:5px;
-  width:25px;
-  &:hover{
-    cursor:pointer;
-    color:white;
-    background-color:#ff8080;
+  display: inline-block;
+  font-size: 17px;
+  font-weight: 600;
+  padding: 5px;
+  border-radius: 5px;
+  width: 25px;
+  &:hover {
+    cursor: pointer;
+    color: white;
+    background-color: #ff8080;
   }
-  &:focus::after{
-    color:white;
-    background-color:#ff8080;
+  &:focus::after {
+    color: white;
+    background-color: #ff8080;
   }
 `;
 
 const PageSpan = styled.span`
   &:hover::after,
-  &:focus::after{
-    border-radius:100%;
-    color:white;
-    background-color:#ff8080;
+  &:focus::after {
+    border-radius: 100%;
+    color: white;
+    background-color: #ff8080;
   }
 `;
 
 const PageBody = styled.div`
-  display:flex;
-  justify-content:flex-end;
-  align-items:center;
-  position:fixed;
-  bottom:5px;
-  right:15px;
-  width:175%;
-`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  position: fixed;
+  bottom: 5px;
+  right: 15px;
+  width: 175%;
+  transition: 0.2s linear;
+`;
 
 const Pagination = ({ postsPerPage, totalPosts, paginate }) => {
   const pageNumbers = [];
   for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
     pageNumbers.push(i);
   }
+
+  const activeIn = document.querySelector(".pagination");
+
+  function open() {
+    activeIn.classList.add("active");
+  }
+
+  function close() {
+    activeIn.classList.remove("active");
+  }
+
   return (
     <div>
       <nav>
-        <PageBody>
-        <PageUl className="pagination">
+        <PageBody className="pagination">
+          <PageUl>
+            <button
+              style={{
+                borderRadius: "5px",
+                border: "2px solid gray",
+                boxSizing: "border-box",
+                position: "absolute",
+                top: "-45px",
+                fontWeight: "700",
+                padding: "10px",
+              }}
+              className="openBtn"
+              onClick={open}
+            >
+              페이지리스트 열기
+            </button>
+            <button
+              style={{ marginRight: "15px",marginLeft:'3px',fontWeight: "700" }}
+              className="openBtn"
+              onClick={close}
+            >
+              숨기기
+            </button>
             Pagination
-          {pageNumbers.map(number => (
-            <PageLi key={number} className="page-item">
-              <PageSpan onClick={() => paginate(number)} className="page-link">
-                {number}
-              </PageSpan>
-            </PageLi>
-          ))}
-        </PageUl>
+            {pageNumbers.map((number) => (
+              <PageLi key={number} className="page-item">
+                <PageSpan
+                  onClick={() => paginate(number)}
+                  className="page-link"
+                >
+                  {number}
+                </PageSpan>
+              </PageLi>
+            ))}
+          </PageUl>
         </PageBody>
       </nav>
     </div>
